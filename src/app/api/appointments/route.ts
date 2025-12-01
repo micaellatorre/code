81fff3a59d3623fcbe2d3e2a74601c3071eb1e13
Micael Latorre <micaellatorre@gmail.com>
2025-12-01 16:36:48 -0300

    Revert "Fix"
    This reverts commit 0f2f119a38a841cd848962c21efbb411435aeef6.

--- src/app/api/appointments/route.ts
+++ src/app/api/appointments/route.ts
@@ -41,9 +41,7 @@ export async function POST(request: Request) {
   }
 
   try {
-    //  En este caso transacción para crear la cita y los intereses asociados
-    // inetereses como: productId, notes, priority y tx deberia tiparse como Prisma.TransactionClient
-    const txResult = await prisma.$transaction(async (tx: any) => {
+    const txResult = await prisma.$transaction(async (tx) => {
       const tenantId = process.env.DEFAULT_TENANT_ID;
       if (!tenantId) {
         throw new Error("DEFAULT_TENANT_ID no configurado");
