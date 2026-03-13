9b6aab3c1a794e4f71a14f342369e0da62278b22
Micael Latorre <micaellatorre@gmail.com>
2026-03-13 01:01:55 -0300

    prisma patch

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,13 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
+  },
   datasource: {
     url: env("DATABASE_URL"),
+    // Optionally support shadow DB if present:
+    // shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
   },
 });
